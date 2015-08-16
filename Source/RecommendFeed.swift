ea67c5642790edafd77e9ebdb532ec88574165c6
Hiroki Kumamoto <kumabook@live.jp>
2015-08-16 22:15:13 +0900

    Add recommend feeds

--- Source/RecommendFeed.swift
+++ Source/RecommendFeed.swift
@@ -12,7 +12,12 @@ public class RecommendFeed {
     public class var ids: [String] {
         struct Static {
             static let ids: [String] = ["feed/http://spincoaster.com/feed",
-                                        "feed/http://matome.naver.jp/feed/topic/1Hinb"]
+                                        "feed/http://matome.naver.jp/feed/topic/1Hinb",
+                                        "feed/http://basement-times.com/feed/",
+                                        "feed/http://uncannyzine.com/feed",
+                                        "feed/http://nichemusic.info/feed/",
+                                        "feed/http://makebelievemelodies.com/?feed=rss2",
+                                        "feed/http://andithereport.com/feed/"]
         }
         return Static.ids
     }
