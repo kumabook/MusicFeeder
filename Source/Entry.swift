2242e6464cd95c073d66f7520d514c33b2615dd9
Hiroki Kumamoto <kumabook@live.jp>
2015-10-13 23:59:04 +0900

    Use Breit.String#passedTime

--- Source/Entry.swift
+++ Source/Entry.swift
@@ -26,27 +26,6 @@ extension Entry {
         } ?? []
     }
     public var passedTime: String {
-        let now           = NSDate()
-        let publishedDate = NSDate(timeIntervalSince1970: NSTimeInterval(Double(published)/1000))
-        let passed        = now.timeIntervalSinceDate(publishedDate)
-        let minute: Int  = Int(passed) / 60
-        if minute <= 1 {
-            return "1 minute ago"
-        }
-        if minute < 60 {
-            return "\(minute)" + " minutes ago"
-        }
-        let hour = minute / 60;
-        if hour <= 1 {
-            return "\(hour)" + " hour ago"
-        }
-        if (hour < 24) {
-            return "\(hour)" + " hours ago"
-        }
-        let day = hour / 24;
-        if day <= 1 {
-            return "\(day)" + " day ago"
-        }
-        return "\(day)" + " days ago"
+        return published.date.passedTime
     }
 }
