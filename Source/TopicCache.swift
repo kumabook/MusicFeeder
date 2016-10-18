9e42aa74f752e09d828a5d1da31a3c7cb58b0279
Hiroki Kumamoto <kumabook@live.jp>
2016-10-18 16:42:01 +0900

    Implement TopicCache: TopicCacheList

---
+++ Source/TopicCache.swift
@@ -0,0 +1,40 @@
+//
+//  TopicCache.swift
+//  MusicFeeder
+//
+//  Created by Hiroki Kumamoto on 10/15/16.
+//  Copyright © 2016 kumabook. All rights reserved.
+//
+
+import Realm
+import FeedlyKit
+import Result
+
+public final class TopicCacheList: RLMObject, CacheList {
+    public typealias Item   = Topic
+    public typealias Object = TopicStore
+    public static var objectClassName: String { return TopicStore.className() }
+    
+    public override class func primaryKey() -> String {
+        return "id"
+    }
+    
+    dynamic public var id:        String   = ""
+    dynamic public var timestamp: Int64    = 0
+    dynamic public var items:     RLMArray = RLMArray(objectClassName: TopicCacheList.objectClassName)
+    
+    public static func deleteAllItems() {
+        materialize(try realm.transactionWithBlock()
+            {
+                realm.deleteObjects(TopicStore.allObjectsInRealm(realm))
+                realm.deleteObjects(allObjectsInRealm(realm))
+            }
+        )
+    }
+}
+
+extension Topic: Cacheable {
+    public func toCacheStoreObject() -> TopicStore {
+        return toStoreObject()
+    }
+}
