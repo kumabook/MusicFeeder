cb6e64d69f229babbbf3bef4a2472c1de8d59819
Hiroki Kumamoto <kumabook@live.jp>
2016-10-30 14:29:27 +0900

    Implement TimeSpecifiedEntryRepository and TimeSpecifiedTrackStreamRepository

---
+++ Source/TimeSpecifiedEntryRepository.swift
@@ -0,0 +1,40 @@
+//
+//  TimeSpecifiedEntryRepository.swift
+//  MusicFeeder
+//
+//  Created by Hiroki Kumamoto on 10/30/16.
+//  Copyright © 2016 kumabook. All rights reserved.
+//
+
+import Foundation
+import FeedlyKit
+
+public class TimeSpecifiedEntryRepository: EntryRepository {
+    var newerThan: Int64
+    var olderThan: Int64
+    
+    public init(stream: Stream, unreadOnly: Bool, perPage: Int, newerThan: Int64, olderThan: Int64) {
+        self.newerThan = newerThan
+        self.olderThan = olderThan
+        super.init(stream: stream, unreadOnly: unreadOnly, perPage: perPage)
+    }
+    
+    public override var paginationParams: MusicFeeder.PaginationParams {
+        let params          = MusicFeeder.PaginationParams()
+        params.continuation = continuation
+        params.unreadOnly   = unreadOnly
+        params.count        = perPage
+        params.newerThan    = newerThan
+        params.olderThan    = olderThan
+        return params
+    }
+    
+    public override var paginationParamsForLatest: MusicFeeder.PaginationParams {
+        let params        = MusicFeeder.PaginationParams()
+        params.unreadOnly = unreadOnly
+        params.count      = perPage
+        params.newerThan  = newerThan
+        params.olderThan  = olderThan
+        return params
+    }
+}
