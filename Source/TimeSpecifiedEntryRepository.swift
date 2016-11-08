3c8cd190e8269681f338d2d280d18a738bfda1d8
Hiroki Kumamoto <kumabook@live.jp>
2016-11-08 19:36:14 +0900

    Add name property to TimeSpecified repositories in order to cache streams separately that have same streamId but different query

--- Source/TimeSpecifiedEntryRepository.swift
+++ Source/TimeSpecifiedEntryRepository.swift
@@ -12,13 +12,20 @@ import FeedlyKit
 public class TimeSpecifiedEntryRepository: EntryRepository {
     var newerThan: Int64
     var olderThan: Int64
-    
-    public init(stream: Stream, unreadOnly: Bool, perPage: Int, newerThan: Int64, olderThan: Int64) {
+
+    var name: String
+
+    override public var cacheKey: String {
+        return "\(stream.streamId)-\(name)"
+    }
+
+    public init(stream: Stream, unreadOnly: Bool, perPage: Int, newerThan: Int64, olderThan: Int64, name: String) {
         self.newerThan = newerThan
         self.olderThan = olderThan
+        self.name      = name
         super.init(stream: stream, unreadOnly: unreadOnly, perPage: perPage)
     }
-    
+
     public override var paginationParams: MusicFeeder.PaginationParams {
         let params          = MusicFeeder.PaginationParams()
         params.continuation = continuation
