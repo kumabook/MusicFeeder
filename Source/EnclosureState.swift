f049b0e41ce65586c8bc44016cf5b23b3c6dbc32
Hiroki Kumamoto <kumabook@live.jp>
2017-03-13 14:37:07 +0900

    Add EnclosureState enum

---
+++ Source/EnclosureState.swift
@@ -0,0 +1,14 @@
+//
+//  EnclosureState.swift
+//  MusicFeeder
+//
+//  Created by Hiroki Kumamoto on 2017/03/12.
+//  Copyright © 2017 kumabook. All rights reserved.
+//
+
+import Foundation
+
+public enum EnclosureState: String {
+    case alive = "alive"
+    case dead  = "dead"
+}
