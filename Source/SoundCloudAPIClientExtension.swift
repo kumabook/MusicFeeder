c1de39e385a79a3e99c57413fb83eacfdb627add
Hiroki Kumamoto <kumabook@live.jp>
2017-04-06 16:51:47 +0900

    Implement helper methods of SoundCloudKit.APIClient that returns SignalProducer

---
+++ Source/SoundCloudAPIClientExtension.swift
@@ -0,0 +1,88 @@
+//
+//  SoundCloudAPIClientExtension.swift
+//  MusicFeeder
+//
+//  Created by Hiroki Kumamoto on 2017/04/06.
+//  Copyright © 2017 kumabook. All rights reserved.
+//
+
+import Foundation
+import SoundCloudKit
+import ReactiveSwift
+import Alamofire
+
+extension SoundCloudKit.APIClient {
+    public func fetchItem<T: JSONInitializable>(_ route: Router) -> SignalProducer<T, NSError> {
+        return SignalProducer { observer, disposable in
+            self.fetchItem(route, callback: { (req: URLRequest?, res: HTTPURLResponse?, result: Result<T>) in
+                switch result {
+                case .success(let value):
+                    observer.send(value: value)
+                    observer.sendCompleted()
+                case .failure(let error):
+                    observer.send(error: error as NSError)
+                }
+            })
+        }
+    }
+    public func fetchItems<T: JSONInitializable>(_ route: Router) -> SignalProducer<[T], NSError> {
+        return SignalProducer { observer, disposable in
+            self.fetchItems(route, callback: { (req: URLRequest?, res: HTTPURLResponse?, result: Result<[T]>) in
+                switch result {
+                case .success(let value):
+                    observer.send(value: value)
+                    observer.sendCompleted()
+                case .failure(let error):
+                    observer.send(error: error as NSError)
+                }
+            })
+        }
+    }
+    public func fetchUsers(_ query: String) -> SignalProducer<[User], NSError> {
+        return fetchItems(Router.users(query))
+    }
+    
+    public func fetchMe() -> SignalProducer<User, NSError> {
+        return fetchItem(Router.me)
+    }
+    
+    public func fetchUser(_ userId: String) -> SignalProducer<User, NSError> {
+        return fetchItem(Router.user(userId))
+    }
+    
+    public func fetchTrack(_ trackId: String) -> SignalProducer<SoundCloudKit.Track, NSError> {
+        return fetchItem(Router.track(trackId))
+    }
+    
+    public func fetchTracksOf(_ user: User) -> SignalProducer<[SoundCloudKit.Track], NSError> {
+        return fetchItems(Router.tracksOfUser(user))
+    }
+    
+    public func fetchPlaylistsOf(_ user: User) -> SignalProducer<[SoundCloudKit.Playlist], NSError> {
+        return fetchItems(Router.playlistsOfUser(user))
+    }
+    
+    public func fetchFollowingsOf(_ user: User) -> SignalProducer<[User], NSError> {
+        return self.fetchItems(Router.followingsOfUser(user))
+    }
+    
+    public func fetchFavoritesOf(_ user: User) -> SignalProducer<[SoundCloudKit.Track], NSError> {
+        return self.fetchItems(Router.favoritesOfUser(user))
+    }
+    
+    public func fetchActivities() -> SignalProducer<ActivityList, NSError> {
+        return self.fetchItem(Router.activities)
+    }
+    
+    public func fetchNextActivities(_ nextHref: String) -> SignalProducer<ActivityList, NSError> {
+        return self.fetchItem(Router.nextActivities(nextHref))
+    }
+    
+    public func fetchLatestActivities(_ futureHref: String) -> SignalProducer<ActivityList, NSError> {
+        return self.fetchItem(Router.futureActivities(futureHref))
+    }
+    
+    public func fetchPlaylist(_ id: Int) -> SignalProducer<SoundCloudKit.Playlist, NSError> {
+        return self.fetchItem(Router.playlist("\(id)"))
+    }
+}
