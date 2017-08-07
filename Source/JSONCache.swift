27df4bf1638aea31e0424bb2be7c9e0fde7b1d4d
Hiroki Kumamoto <kumabook@live.jp>
2017-08-07 14:49:43 +0900

    Revise JSONCache with hyperoslo/Cache

--- Source/JSONCache.swift
+++ Source/JSONCache.swift
@@ -9,53 +9,32 @@
 import Foundation
 import Realm
 import FeedlyKit
-import SwiftyJSON
 import Result
+import Cache
 
-public class JSONItem: Cacheable {
-    public typealias Object = JSONObject
-    public var id:    String    = ""
-    public var value: String = ""
-    public init(id: String, value: String) {
-        self.id    = id
-        self.value = value
-    }
-    public func toCacheStoreObject(_ realm: RLMRealm) -> JSONObject {
-        return JSONObject(value: ["id": id, "value": value])
-    }
-}
+public final class JSONCache {
+    public var cacheLifetimeSec: TimeInterval
+    let cache = SpecializedCache<String>(name: "JSONCache")
 
+    public static var shared: JSONCache = JSONCache()
 
-public class JSONObject: RLMObject {
-    public dynamic var id:    String = ""
-    public dynamic var value: String = ""
-    public override class func primaryKey() -> String {
-        return "id"
+    public init(cacheLifetimeSec: TimeInterval = 60 * 60 * 24 * 3) {
+        self.cacheLifetimeSec = cacheLifetimeSec
     }
-    public override class func requiredProperties() -> [String] {
-        return ["id", "value"]
-    }
-}
 
-public final class JSONCacheSet: RLMObject, CacheSet {
-    public typealias Item   = JSONItem
-    public typealias Object = JSONObject
-    public typealias Entity = JSONCacheEntity
-    public static var objectClassName: String { return JSONObject.className() }
-    public override class func requiredProperties() -> [String] {
-        return ["id"]
+    public func add(_ jsonString: String, forKey: String) throws {
+        try cache.addObject(jsonString, forKey: forKey, expiry: Expiry.seconds(cacheLifetimeSec))
     }
-}
 
-public final class JSONCacheEntity: RLMObject, CacheEntity {
-    public typealias Object = JSONObject
-    dynamic public var id:        String   = ""
-    dynamic public var timestamp: Int64    = 0
-    dynamic public var item:      JSONObject?
-    public override class func primaryKey() -> String {
-        return "id"
+    public func get(forKey: String) -> String? {
+        return cache.object(forKey: forKey)
     }
-    public override class func requiredProperties() -> [String] {
-        return ["id"]
+
+    public func clearExpired() {
+        try? cache.clearExpired()
+    }
+
+    public func cachedDiskSize() -> UInt64 {
+        return (try? cache.totalDiskSize()) ?? 0
     }
 }
